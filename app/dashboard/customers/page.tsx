7283c6b1b62e6b7e9668db451ba5d26b9394eced
gtn-74 <r_ogitani@bbmedia.co.jp>
2024-09-03 15:24:07 +0900

    不要ページ削除

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,45 +1,7 @@
 'use client';
 
-import { TestBox } from '@/app/global';
-import { Box, useMediaQuery } from '@mui/material';
-
 export default function Page() {
-  const isSmallDevice = useMediaQuery('(max-width: 768px)');
-
-  return (
-    <>
-      {/* useMediaQueryでコンポーネントの出しわけ */}
-      {/* {isSmallDevice ? (
-        <Box sx={{ backgroundColor: 'red', color: 'white' }} width={'500px'}>
-          header
-        </Box>
-      ) : (
-        <Box
-          sx={{
-            backgroundColor: 'red',
-            color: 'white',
-            writingMode: 'vertical-rl',
-          }}
-          height={'500px'}
-        >
-          header
-        </Box>
-      )} */}
-      {/* MUIのthemeを使って切り替え */}
-      {/* デフォのthemeが生きる */}
-      {/* <Box
-        sx={{
-          backgroundColor: { sm: 'blue', lg: 'yellow', xl: 'green' },
-          color: { sm: 'white', lg: 'black', xl: 'white' },
-        }}
-        width={'100vw'}
-      >
-        a
-      </Box> */}
-      {/* style.tsに逃す */}
-      <TestBox>a</TestBox>
-    </>
-  );
+  return <div>a</div>;
 }
 
 /*
